c03649075c326ef92fe9ff98554eb9b994e71972
SeregaBuyanov <kekwpepegys@gmail.com>
2022-03-23 16:35:19 +0400

    Практические

---
+++ web делефоперс/пр11/script.js
@@ -0,0 +1,10 @@
+function show_hide(obj_id){
+doc=document.getElementById(obj_id);
+if(doc.style.display == "none") doc.style.display = "block";
+else doc.style.display = "none"
+}
+function check(){
+calc.js
+if ( calc.answer.value == "Сортавала" )
+  alert("харош");
+else alert("неправильно ");}
